d7dc40a88e3f83090a5231ab133d90d8298b63ef
Kawano Yoshitaka <y.kawano0723@gmail.com>
2020-10-08 18:41:29 +0900

    refactor backpropagation method

--- project-bikesharing/my_answers.py
+++ project-bikesharing/my_answers.py
@@ -90,12 +90,12 @@ class NeuralNetwork(object):
         # TODO: Output error - Replace this value with your calculations.
         error = y - final_outputs # Output layer error is the difference between desired target and actual output.
         
-        # TODO: Calculate the hidden layer's contribution to the error
-        hidden_error = np.dot(self.weights_hidden_to_output, error) 
-        
         # TODO: Backpropagated error terms - Replace these values with your calculations.
         output_error_term = error
         
+        # TODO: Calculate the hidden layer's contribution to the error
+        hidden_error = np.dot(output_error_term, self.weights_hidden_to_output.T)
+
         hidden_error_term = hidden_error * hidden_outputs * (1 - hidden_outputs)
         
         # Weight step (input to hidden)
